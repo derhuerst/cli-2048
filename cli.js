7f7dca6059508276f7f1dd9f54a4d1871e52adae
Jannis R <mail@jannisr.de>
2017-06-25 01:29:26 +0200

    CLI interface

--- cli.js
+++ cli.js
@@ -1,4 +1,25 @@
 #!/usr/bin/env node
 'use strict'
 
-// todo
+const minimist = require('minimist')
+
+const pkg = require('./package.json')
+const game = require('.')
+
+const argv = minimist(process.argv.slice(2))
+
+if (argv.help || argv.h) {
+	process.stdout.write(`
+Usage:
+    2048
+\n`)
+	process.exit(0)
+}
+
+if (argv.version || argv.v) {
+	process.stdout.write(`cli-2048 v${pkg.version}\n`)
+	process.exit(0)
+}
+
+game()
+.on('abort', () => process.exit(1))
